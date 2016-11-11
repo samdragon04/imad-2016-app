f8b2a7ad83846a9d3de83a236aaedb45f85c6e61
samdragon04 <samdragon04@gmail.com>
2016-11-11 15:58:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,18 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-1', function (req, res) {
-  res.send('Article 1 requested will be served here');
-});
-
-app.get('/article-2', function (req, res) {
-  res.send('Article 2 requested will be served here');
-});
-
-app.get('/article-3', function (req, res) {
-  res.send('Article 3 requested will be served here');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
