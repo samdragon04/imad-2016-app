793adc605723f01e4738d0116771a89559fa5c70
samdragon04 <samdragon04@gmail.com>
2016-11-17 19:25:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/ui/index', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/Ackno', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Ackno.html'));
+});
+
+app.get('/ui/comingsoon', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'comingsoon.html'));
+});
+
+app.get('/ui/contact', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -15,6 +27,14 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/2-banner.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '2-banner.png'));
+});
+
+app.get('/ui/comingsoon', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'comingsoon.png'));
+});
+
+app.get('/ui/thankyou', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'thankyou.png'));
 });
 
 
